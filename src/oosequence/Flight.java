41ca8af0791450b3cd196cb0b7dfaa2d80fea5a9
Seyar Ghulom Hazrat <Seyar.GhulomHazrat@ucalgary.ca>
2023-03-09 23:37:48 +0000

    CCIntroClasses after completing 4a on CC instructions i.e finishing first constructor requirements.

--- src/oosequence/Flight.java
+++ src/oosequence/Flight.java
@@ -7,6 +7,32 @@ public class Flight {
 	Date depart;
 	
 	public Flight(Date depart, Date arrive){
+		//If either argument (or both) is null, you can use the arguments to set the instance variables directly
+		if (arrive == null && depart == null) {
+			//then flight is null
+			this.depart = null;
+			this.arrive = null;
+		}
+		if (depart == null) {
+			//can set arrival in condition
+			this.depart = null;
+			this.arrive = new Date(arrive.getTime());
+		}
+		if (arrive == null) {
+			//can set depart in condition
+			this.depart = new Date (depart.getTime());
+			this.arrive = null;
+		}
+		// check if departure is prior to arrival
+		if (depart.before(arrive)) {
+			this.depart = new Date (depart.getTime());
+			this.arrive = new Date (arrive.getTime());
+		}
+		//else invalid flight
+		else {
+			this.depart = null;
+			this.arrive = null;
+		}
 		
 	}
 	
