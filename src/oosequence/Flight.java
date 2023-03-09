d808eb7065811d363b7fe12761fd923d9ee633b0
Seyar Ghulom Hazrat <Seyar.GhulomHazrat@ucalgary.ca>
2023-03-09 23:14:13 +0000

    CCIntroClasses after implementing initial requirements.

---
+++ src/oosequence/Flight.java
@@ -0,0 +1,20 @@
+package oosequence;
+
+import java.util.Date;
+
+public class Flight {
+	Date arrive;
+	Date depart;
+	
+	public Flight(Date depart, Date arrive){
+		
+	}
+	
+	public Flight(Flight flight) {
+		
+	}
+	
+	public long length() {
+		return 0;
+	}
+}
