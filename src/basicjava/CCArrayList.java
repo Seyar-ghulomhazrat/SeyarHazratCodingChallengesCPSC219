6ec3a33228c5598897f732924d96ad3ddba8081e
Seyar Ghulom Hazrat <Seyar.GhulomHazrat@ucalgary.ca>
2023-03-14 21:32:21 +0000

    Grade Calc project after ArrayList passes all tests.

--- src/basicjava/CCArrayList.java
+++ src/basicjava/CCArrayList.java
@@ -4,8 +4,24 @@ import java.util.ArrayList;
 
 public class CCArrayList {
 	public static int indexOfIgnoreCase(ArrayList<String> strs, String toFind) {
-		return 0;
+		int size = strs.size();
+
+		for(int i=0; i<size;i+=1) {
+			String stringInList = strs.get(i);
+
+			String replacement = toFind;
+
+			if (stringInList.toLowerCase().equals(replacement.toLowerCase())) {
+				return i;
+			}
+		}
+		return -1;
 	}
 	public static void insert(ArrayList<Double> nums, double numToInsert, int insertAtIndex) {
+		// check if insert at index is a valid number
+		if (insertAtIndex >= 0 && insertAtIndex <= nums.size()) {
+			// .add moves everything one to the right
+			nums.add(insertAtIndex, numToInsert);
+		}
 	}
 }
